429d5f8d1c500f8d2e630942212ad3fc8a40aae0
Gediminas Paulauskas <menesis@pov.lt>
2011-11-30 10:14:59 +0000

    Back to development: 3.6.5

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name = 'zope.keyreference',
-      version='3.6.4',
+      version='3.6.5dev',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Key References',
