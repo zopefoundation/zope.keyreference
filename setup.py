40af3e1ca39ae0c6c2ce1bbf44d6ccec18252b87
Gediminas Paulauskas <menesis@pov.lt>
2011-11-30 10:13:23 +0000

    Preparing release 3.6.4

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name = 'zope.keyreference',
-      version='3.6.3',
+      version='3.6.4',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Key References',
